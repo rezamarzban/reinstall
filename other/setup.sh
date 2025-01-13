b8f272d96deea16273a2bb0314c05d75f78e7b65
rezamarzban <73051714+rezamarzban@users.noreply.github.com>
2025-01-13 10:02:41 +0330

    Update setup.sh

--- other/setup.sh
+++ other/setup.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+git clone https://github.com/rezamarzban/reinstall
+cd reinstall
+cd other
+chmod +x *.sh
+
 # Fetch VPS details
 MAC_ADDRESS=$(ip link | awk '/ether/ {print $2; exit}')
 IP_ADDRESS=$(ip -o -f inet addr show | awk '/scope global/ {print $4; exit}')
@@ -23,3 +28,15 @@ else
     echo "File $INIT_FILE not found!"
     exit 1
 fi
+
+mkdir tmp
+cd tmp
+zcat ../initramfs-virt | cpio -idm
+install -Dm0755 ../init.sh init
+cp ../initrd-network.sh .
+cp ../trans.sh .
+cat ../dhcpcd-openrc-10.1.0-r0.apk | tar xz --warning=no-unknown-keyword -C .
+find . | cpio --quiet -o -H newc | gzip -1 > /reinstall-initrd
+cp ../vmlinuz-virt /reinstall-vmlinuz
+cp ../custom.cfg /boot/grub/.
+cp ../grubenv /boot/grub/.
